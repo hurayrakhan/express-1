9c1f197b7d8f72225d174c1de29dd22a6a26af86
Hurayra Khan <imhurayrakhan@gmail.com>
2026-06-02 23:08:10 +0600

    feat: added users post api

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express, { type Application, type Request, type Response } from 'express';
 import { Pool } from 'pg';
+import config from './config';
 
 const app : Application = express();
-const PORT = 5000;
+const PORT = config.port || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +11,7 @@ app.use(express.text());
 
 // database connection
 const pool = new Pool({
-    connectionString: "postgresql://neondb_owner:npg_KgQ4Za6vrSsk@ep-falling-tooth-ao0waccg-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    connectionString: config.connectionString
 })
 
 
@@ -26,13 +27,15 @@ const initDB = async () => {
             is_active BOOLEAN DEFAULT true,
 
             created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
+            updated_at TIMESTAMP DEFAULT NOW(),
             )
             `)
     } catch (error) {
         console.log("Error initializing database: ", error)
     }
 }
+
+initDB();
 
 
 app.get("/", (req : Request, res : Response) => {
@@ -42,12 +45,30 @@ app.get("/", (req : Request, res : Response) => {
     })
 })
 
-app.post("/user", (req : Request , res : Response) => {
-    
-})
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
 
 
+app.post('/user', async (req: Request, res: Response) => {
+    const { name, email, password, is_admin, is_active} = req.body;
+    try {
+        const result = await pool.query(`
+            INSERT INTO users (name, email, password, is_admin, is_active)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING *
+        `, [name, email, password, is_admin, is_active]);
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            user: result.rows[0],
+        });
+    } catch (error: any) {
+        console.error("Error creating user: ", error);
+        res.status(500).json({ 
+            success: false,
+            message: error.message || "Internal Server Error",
+            error: error ,
+        });
+    }
+})
