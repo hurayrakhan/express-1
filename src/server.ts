bc0df8984594c601e508bb8f2b026d0bb3b8084e
Hurayra Khan <imhurayrakhan@gmail.com>
2026-06-03 00:19:19 +0600

    feat: created user delete api endpoint

--- src/server.ts
+++ src/server.ts
@@ -122,3 +122,33 @@ app.get("/users/:id", async (req: Request, res: Response ) => {
         })
     }
 })
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(`
+            DELETE FROM users WHERE id = $1 RETURNING *
+        `, [id]);
+
+       if (result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found!",
+                user: null
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User deleted successfully!",
+
+        })
+    } catch (error : any) {
+        console.error("Error deleting user: ", error);
+        res.status(500).json({
+            success: false,
+            message: error.message || "Internal Server Error",
+            error: error,
+        })
+    }
+});
